708f604d0be6142d6b51a0d016a6b1afffe93878
virendra <vpsk05@gmail.com>
2017-02-24 00:21:37 +0530

    html learning

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	entry: './src/app.js',
 	output: {
-		filename: './dist/app.bundle.js'
-	}
+		path: 'dist',
+		filename: 'app.bundle.js'
+	},
+	plugins: [new HtmlWebpackPlugin({
+		title: 'My project',
+		minify: {
+			collapseWhitespace: true
+		},
+		hash: true,
+		template: './src/index.ejs',
+	})]
 }
